890aa4fc7510ac90b20fa2598c9518e083e81808
liuxiangwin <liuxiangwin@163.com>
2014-10-06 17:19:40 +0800

    Update the operation alogrithm

--- Algorithm/Test_Algorithm-Liuxiangwin/leetcode/com/liuxiangwin/algor/leetcode/test/operator/SingleNumber.java
+++ Algorithm/Test_Algorithm-Liuxiangwin/leetcode/com/liuxiangwin/algor/leetcode/test/operator/SingleNumber.java
@@ -26,5 +26,39 @@ public class SingleNumber {
 		//int[] A = new int[] { 1, 2, 1, 2, 3, 4, 4 };
 		int[] A = new int[] { 1,1, 2, 1, 2, 4, 4,4 };
 		System.out.println(slt.singleNumber(A));
+		
+		System.out.println(getBitByOrder(10, 1));
+		System.out.println(getBitByOrder(10, 2));
+		System.out.println(getBitByOrder(10, 3));
+		
+    }
+
+	private static void xorOperation() {
+		//XOr operation
+		System.out.println(1^0^1);
+		System.out.println(0^1);
+		System.out.println(1^1);
+	}
+
+	private static void orOperation() {
+		//Or operation 有1就是1
+		System.out.println(1|0);
+		System.out.println(0|1);
+		System.out.println(1|1);
+		System.out.println();
+	}
+
+	private static void andOperation() {
+		//And operation 只有1 才是1
+		System.out.println(1&1);
+		System.out.println(0&1);
+		System.out.println(0&0);
+		System.out.println();
+	}
+    
+    public static int getBitByOrder(int num, int i){
+    	int result = num & (1<<i);
+        return result;
+    	
     }
 }
