503ce3328675428b520e269a45403eef5807e043
liuxiangwin <liuxiangwin@163.com>
2014-10-08 10:07:46 +0800

    Update tmp file

---
+++ Algorithm/Test_Algorithm-Liuxiangwin/leetcode/com/liuxiangwin/algor/leetcode/test/tmp/TestIntToRoman.java
@@ -0,0 +1,29 @@
+package com.liuxiangwin.algor.leetcode.test.tmp;
+
+public class TestIntToRoman {
+
+	public static void main(String args[]) {
+		
+		TestIntToRoman testIntToRoman = new TestIntToRoman();
+	}
+
+	public String intToRoman(int num) {
+		int[] base = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+		String[] roman = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X",
+				"IX", "V", "IV", "I" };
+
+		String result = "";
+
+		for (int i = 0; num > 0; i++) {
+			int count = num / base[i];
+			num %= base[i];
+
+			while (count > 0) {
+				result += roman[i];
+				--count;
+			}
+		}
+
+		return result;
+	}
+}
