5ae926a690744189c0d375374c297228b30afd5f
liuxiangwin <liuxiangwin@163.com>
2014-10-08 12:05:58 +0800

    Update  TreeNode

---
+++ Algorithm/Test_Algorithm-Liuxiangwin/leetcode/com/liuxiangwin/algor/leetcode/uitl/TreeNode.java
@@ -0,0 +1,13 @@
+package com.liuxiangwin.algor.leetcode.uitl;
+
+
+public class TreeNode {
+	public int val;
+	public TreeNode left;
+	public TreeNode right;
+
+	public TreeNode(int x) {
+		val = x;
+	}
+
+}
